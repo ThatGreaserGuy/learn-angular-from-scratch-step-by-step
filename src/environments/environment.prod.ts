c055d7d0edf32a3b397242b56ef90254ae7a7303
ThatGreaserGuy <ryan.rieth@snhu.edu>
2023-10-01 23:19:36 -0400

    Fixed a typo!!

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://9a2med2zj3.execute-api.us-east-1.amazonaws.com/'
+  api_url: 'https://9a2med2zj3.execute-api.us-east-1.amazonaws.com'
 };
